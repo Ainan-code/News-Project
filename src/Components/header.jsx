130ab8d1508435a9d803f2602c6c826d5b57e9b0
Ainan <ainanhassan123@gmail.com>
2023-11-04 18:49:17 +0300

    initial commit

---
+++ src/Components/header.jsx
@@ -0,0 +1,32 @@
+function Header() {
+    return (
+
+     
+<nav id="navbar" class="">
+  <div class="nav-wrapper">
+
+    <div class="logo">
+     
+      <a href="#home"><i class="fa fa-angellist"></i> NewsHour</a>
+    </div>
+
+   
+    <ul id="menu">
+      <li><a href="#home">Home</a></li>
+   <li><a href="#services">Politics</a></li>
+   <li><a href="#about">Science</a></li>
+   <li><a href="#contact">Sport</a></li>
+    </ul>
+  </div>
+
+
+
+
+
+</nav>
+    
+    )
+}
+
+
+export default Header;
---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+
+
+
+
+const initialState = {
+  name: "Ainan",
+  age: 26 ,
+  Newsdata: [],
+  cart: [],
+  
+
+}
+
+
+
+
+
+
+
+export default initialState;
